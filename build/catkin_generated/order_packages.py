8fe4412eec1d1af3d5e52121f463b1f434209cf8
xiaotong <h13021025522@163.com>
2020-03-27 12:40:14 +0800

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/hxt/catkin_my/cat_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/hxt/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/hxt/catkin_ws/devel;/opt/ros/kinetic" != "" else []
